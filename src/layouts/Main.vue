6b635259f84183c022e131f45d644c09e516fd38
Yauheni <yauhenikozak@gmail.com>
2026-01-06 14:26:06 +0300

    fix

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -4,7 +4,6 @@ import { useRoute } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
 import BottomNavigation from '@/components/BottomNavigation.vue'
-import DevToolsButton from '@/components/DevToolsButton.vue'
 
 const route = useRoute()
 const userStore = useUserStore()
@@ -25,7 +24,6 @@ const showBottomNavigation = computed(() => {
       ]" 
     />
     <BottomNavigation v-if="showBottomNavigation" />
-    <DevToolsButton />
   </div>
 </template>
 
